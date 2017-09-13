f231cea5ea8ec946914973477a4ee8a0dcb6f6cc
Monique Moledo <moniquemoledo@hotmail.com>
2017-09-13 13:25:57 -0300

    :sparkles: Use CustomerValidation in the api

--- api.py
+++ api.py
@@ -1,15 +1,27 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from validator import CustomerValidator
 
 app = Flask(__name__)
 api = Api(app)
+validator = CustomerValidator()
 
 
 class CustomerValidation(Resource):
     def get(self):
-        url = ""
+        url = ''
         invalid_customers = []
         response = {}
+
+        number_of_pages = validator.get_api_number_of_pages(url)
+        for page in range(1, number_of_pages + 1):
+
+            customers, validations = validator.get_customers_and_validations(url, page)
+
+            validation_result = validator.get_customer_validation(customers, validations)
+
+            invalid_customers.extend(validation_result)
+
         response["invalid_customers"] = invalid_customers
 
         return response
