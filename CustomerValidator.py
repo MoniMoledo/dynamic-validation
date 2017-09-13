a5d687396dd595c90948164dc32e6953c70dede5
Monique Moledo <moniquemoledo@hotmail.com>
2017-09-13 13:04:59 -0300

    :sparkles: Create CustomerValidator

---
+++ CustomerValidator.py
@@ -0,0 +1,36 @@
+import requests
+import math
+import json
+from JsonSchema import JsonSchema
+
+
+class CustomerValidator:
+
+    json_schema = JsonSchema()
+
+    def get_api_number_of_pages(self, url):
+        response = requests.get(url=url)
+        data = response.json()
+
+        number_of_customers = float(data["pagination"]["total"])
+        customers_per_page = float(data["pagination"]["per_page"])
+        float_number_of_pages = number_of_customers/customers_per_page
+        number_of_pages = int(math.ceil(float_number_of_pages))
+
+        return number_of_pages
+
+    def get_customers_and_validations(self, url, page):
+        params = dict(page=page)
+        response = requests.get(url=url, params=params).json()
+        
+        return response["customers"], response["validations"]
+
+    def get_customer_validation(self, customers, validations):
+        invalid_customers = []
+        schema = self.json_schema.create_schema(validations)
+
+        for customer in customers:
+            validation_result = self.json_schema.validate(schema, customer)
+            if(any(validation_result)):
+                invalid_customers.append(validation_result)
+        return invalid_customers
